0c558fdcb23e20c0657a29b8e10d2510b649f2fd
nermenmoussa <nermendawood950@gmail.com>
2026-05-31 03:45:03 +0300

    feat-delete prescription

--- src/modules/prescrption/prescription.controller.js
+++ src/modules/prescrption/prescription.controller.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
-import *as PS from "../prescrption/prescription.service.js"
+import * as PS from "../prescrption/prescription.service.js"
 import * as PV from "../prescrption/prescription.validation.js"
 
 import { validation } from "../../common/middleware/validation.js";
 import { authentication } from "../../common/middleware/authenticataiaon.js";
 import { multer_host } from "../../common/middleware/multer.js";
 import { multerenum } from "../../common/enum/multerenum.js";
-const prescrptionrouter = Router()
+
+const authcontroller = require("../../common/middleware/authenticataiaon.js")
+const prescrptionrouter = express.Router()
 
 //api
+
+prescrptionrouter.route('/:id').delete(authcontroller.protect,authcontroller.restrictTo("doctor","admin"),PS.deleteprescrption)
 
 export default prescrptionrouter
--- src/modules/prescrption/prescription.service.js
+++ src/modules/prescrption/prescription.service.js
@@ -3,3 +3,21 @@ import prescrptionmodel from "../../DB/models/prescriptionmodel.js";
 import cloudinary from "../../common/utilits/cloudinary.js";
 import * as db_service from "../../DB/db.service.js";
 import { successresponse } from "../../common/utilits/responce.success.js"
+
+import { catchAsync } from "../../common/utilits/catchAsync.js";
+import AppError from "../../common/utilits/appError.js";
+
+
+
+
+
+export const deleteprescrption = catchAsync(async (req, res,next) => {
+const prescription = await prescrptionmodel.findByIdAndUpdate(req.params.id,{status:"cancelled",},{new:true})
+if (!prescription) {
+    return next(new AppError('No prescription found with that ID', 404));
+  }
+successresponse({ res, message: "prescription deleted successfully" })
+
+
+}
+)
