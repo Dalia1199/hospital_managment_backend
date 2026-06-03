1d04207398463251ba8e7e72ccfbd64aad17662a
honda4coding <mhndahmd820@gmail.com>
2026-06-03 22:26:27 +0300

    feat: implement get pending doctors endpoint for admin

--- src/modules/admin/admin.controller.js
+++ src/modules/admin/admin.controller.js
@@ -5,8 +5,15 @@ import * as authenticationV from "../admin/admin.validation.js"
 import { validation } from "../../common/middleware/validation.js";
 import { authentication } from "../../common/middleware/authenticataiaon.js";
 import { authorization } from "../../common/middleware/authorization.js";
+import { roleenum } from "../../common/enum/user.enum.js";
 const adminrouter = Router()
 
 //routes
+adminrouter.get(
+    "/doctors/pending",
+    authentication,
+    authorization([roleenum.admin]),
+    AS.getPendingDoctors
+);
 
 export default adminrouter
