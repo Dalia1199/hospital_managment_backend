34b8774544b28b926f0a448e9aac9e1858cba5c7
honda4coding <mhndahmd820@gmail.com>
2026-06-04 15:22:26 +0300

    fix: handle errors in getPendingDoctors

--- src/modules/admin/admin.service.js
+++ src/modules/admin/admin.service.js
@@ -2,31 +2,28 @@ import usermodel from "../../DB/models/usermodel.js";
 import { successresponse } from "../../common/utilits/responce.success.js";
 import { roleenum } from "../../common/enum/user.enum.js";
 import * as db_service from "../../DB/db.service.js";
-import { options } from "joi";
-
-
-
 
 export const getPendingDoctors = async (req, res, next) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
 
-
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
-
-
-    const pendingDoctors = await db_service.find({
-        model: usermodel,
-        filter: { 
-            role: roleenum.doctor,
-            status: "pending"
-        }, 
-        options: {
-            skip,
-            limit,
-            select: "-password",
-            sort: { createdAt: 1 }
-        }
-    });
-    return successresponse({ res, data: pendingDoctors });
+        const pendingDoctors = await db_service.find({
+            model: usermodel,
+            filter: { 
+                role: roleenum.doctor,
+                status: "pending"
+            }, 
+            options: {
+                skip,
+                limit,
+                select: "-password",
+                sort: { createdAt: 1 }
+            }
+        });
+        return successresponse({ res, data: pendingDoctors });
+    } catch (error) {
+        next(error);
+    }
 };
