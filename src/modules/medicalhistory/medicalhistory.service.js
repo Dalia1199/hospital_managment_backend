0af922d51bf20b3a3c9f6b807244774288477893
taha-ahmed-hodhod <121146428+taha-ahmed-hodhod@users.noreply.github.com>
2026-06-12 13:51:18 +0300

    Feat/prespection to medical history (#27)
    * feat/Add Prescription to medical history
    
    * fix/Visible when fetching medical history
    
    * Visible prescription when fetching medical history

--- src/modules/medicalhistory/medicalhistory.service.js
+++ src/modules/medicalhistory/medicalhistory.service.js
@@ -44,8 +44,8 @@ export const getMedicalHistory = async (req, res, next) => {
     const history = await medicalhistorymodel
         .findOne({ patientId })
         .populate("answers")
-        .populate("doctorId");
-
+        .populate("doctorId")
+        .populate("prescriptions");
     successresponse({
         res,
         data: history
